0178764426abd898ca8e6594feab5b434714eb7d
anonymsh <bungamaaf75@gmail.com>
2026-07-30 07:20:52 +0700

    Refactor afterPluginInstall.js for better promise handling

--- hooks/afterPluginInstall.js
+++ hooks/afterPluginInstall.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-
-    let fs = context.requireCordovaModule('fs');
-    let path = context.requireCordovaModule('path');
-    let deferral = context.requireCordovaModule('q').defer();
+    let fs = require('fs');
+    let path = require('path');
+    let Q;
+    try {
+        Q = require('q');
+    } catch (e) {
+        Q = global.Q;
+    }
+    let deferral = Q ? Q.defer() : Promise.defer ? Promise.defer() : (() => {
+        let resolve, reject;
+        let p = new Promise((res, rej) => { resolve = res; reject = rej; });
+        return { promise: p, resolve: resolve, reject: reject };
+    })();
 
     // android platform directory
     let platformAndroidDir = path.join(context.opts.projectRoot, 'platforms/android');
@@ -26,16 +35,22 @@ module.exports = function (context) {
     }
 
     if (fs.existsSync(androidManifestFile)) {
-
         fs.readFile(androidManifestFile, 'UTF-8', function (err, data) {
             if (err) {
                 deferral.reject(err);
+                return;
             }
             var mainActReg = /<activity.+name="MainActivity".+>/;
-            var actString = data.match(mainActReg)[0];
+            var matchResult = data.match(mainActReg);
+            
+            if (!matchResult) {
+                deferral.resolve();
+                return;
+            }
+
+            var actString = matchResult[0];
 
             var test = changeProperty(actString, "configChanges", "orientation|keyboardHidden|keyboard|screenSize|locale", true);
-            //changeProperty(actString, "launchMode", "singleTask", true);
             test = changeProperty(test, "supportsPictureInPicture", "true", false);
             test = changeProperty(test, "resizeableActivity", "true", false);
 
@@ -44,11 +59,14 @@ module.exports = function (context) {
             fs.writeFile(androidManifestFile, finalData, 'UTF-8', function (err) {
                 if (err) {
                     deferral.reject(err);
+                } else {
+                    deferral.resolve();
                 }
-                deferral.resolve();
-            })
+            });
         });
+    } else {
+        deferral.resolve();
     }
 
-    return deferral;
+    return deferral.promise || deferral;
 };
